d307dc045509e906bfb4d440c1e71b7fd708698d
Yash708262 <yash708262@gmail.com>
2025-09-05 22:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-//your JS code here. If required.
+// Helper functions to get and set cookies
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+// Apply preferences on load
+window.onload = function () {
+  const savedFontSize = getCookie("fontsize");
+  const savedFontColor = getCookie("fontcolor");
+
+  if (savedFontSize) {
+    document.documentElement.style.setProperty("--fontsize", savedFontSize + "px");
+    document.getElementById("fontsize").value = savedFontSize;
+  }
+
+  if (savedFontColor) {
+    document.documentElement.style.setProperty("--fontcolor", savedFontColor);
+    document.getElementById("fontcolor").value = savedFontColor;
+  }
+};
+
+// Save preferences on form submit
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault(); // stop page reload
+
+  const fontSize = document.getElementById("fontsize").value;
+  const fontColor = document.getElementById("fontcolor").value;
+
+  // Save cookies for 7 days
+  setCookie("fontsize", fontSize, 7);
+  setCookie("fontcolor", fontColor, 7);
+
+  // Apply
+
